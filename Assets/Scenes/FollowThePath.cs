dd271b20c15b46b2ad32629217872cea08ad2ed7
danielBoger <boger@unochapeco.edu.br>
2019-09-28 10:46:51 -0300

    moving things

---
+++ Assets/Scenes/FollowThePath.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowThePath : MonoBehaviour {
+
+    // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] waypoints;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int waypointIndex = 0;
+
+	// Use this for initialization
+	private void Start () {
+
+        // Set position of Enemy as position of the first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+	}
+
+	// Update is called once per frame
+	private void Update () {
+					void OnCollisionEnter2D(Collision2D collision)
+					{
+					if (collision.gameObject.tag == "mailman")
+					{
+							collision.gameObject.SendMessage("ApplyDamage", 10);
+					}
+
+        // Move Enemy
+        Move();
+	}
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypoints[waypointIndex].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/script/dog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dog : MonoBehaviour
+{
+    public GameObject mailman;
+    public float movementSpeed = 4;
+    void Update()
+    {
+        transform.LookAt(mailman.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+}
